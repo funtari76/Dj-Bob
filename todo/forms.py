119260ddbefc679f6816bd7e21f5f7dfae661df2
funtari76 <funtari777@gmail.com>
2022-08-10 11:03:41 +0000

    had alot of errors didn tmakeit on time

---
+++ todo/forms.py
@@ -0,0 +1,8 @@
+#from django import forms
+f#rom .models import Item
+
+
+#class ItemForm(forms.ModelForm): 
+ #   class Meta:
+  #      model = Item
+   #     fields = ['name', 'done']
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from .models import Item
+#from .forms import ItemForm
 
 # Create your views here.
 def get_todo_list(request):
-    return render(request, 'todo/todo_list.html')
+  item = Item(request, 'todo/todo_list.html')
+  items = Item.objects.all()
+  context = {
+        'items': items
+}
+
+  return render(request, 'todo/todo_list.html', context)
+
+  def add_item(request):
+    if request.method == 'POST':
+    
+ # return redirect('get_movie_list')
+   # form = ItemForm()
+    #context = {
+     #   'form': form
+    #}
+    #return render(request, 'todo/add_item.html', 
+    #context)
